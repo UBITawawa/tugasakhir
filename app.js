ab6d6b9be190f9804a87c459d44296c801157f9c
Edwin <edwin@debian>
2016-12-03 17:52:33 +0700

    Add redis as dependencies

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var redis = require('redis');
 var api = require('./routes/api');
-
 var app = express();
 
+app.set('views',__dirname+'/views');
+app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(__dirname+'/public'));
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var redis = require('redis');
+var redisClient = redis.createClient();
 
 router.get('/getimages', function(req,res){
 	res.send('OK');
 });
+
+
 
 module.exports=router;
