70ee3e7cfb9daf88e89efe46b6b85ec15546b860
RMS46 <edwin.pikachu@gmail.com>
2016-12-29 22:43:28 +0700

    Coba UPNP 2

--- client.js
+++ client.js
@@ -1,23 +1,32 @@
 var Client = require('node-ssdp').Client
    , client = new Client();
 
-client.on('response', function (headers, statusCode, rinfo) {
-	console.log('Got a response to an m-search.');
-});
+var npmredis = require('redis');
+var redis = npmredis.createClient();
 
-client.on('response', function inResponse(headers, code, rinfo) {
-  console.log('Got a response to an m-search:\n%d\n%s\n%s', code, JSON.stringify(headers, null, '  '), JSON.stringify(rinfo, null, '  '))
+client.on('response', function (headers, code, rinfo) {
+ 
+	/*
+	* Saya ambil identifier dari USN
+	* Most Likely bentuk stringnya seperti dibawah
+	* urn:schemas-upnp-org:device:Presentation:1
+	* Saya split by ':', lalu saya ambil indeks ke 3
+	*/
+	var arr = headers.ST.split(":").map(function (val) {
+	  		return val;
+	});
+	var identifier = arr.length > 2? arr[3]:'root';
+	var location = headers.LOCATION; //Base_URL
+	var statusCode = code;
+
+  	redis.hmset(identifier, 'location', location, 'statusCode', statusCode);
 })
 
-// Or get a list of all services on the network
+
 client.search('ssdp:all');
 
-// Or maybe if you want to scour for everything after 5 seconds
+console.log('searching');
+
 setTimeout(function() {
   client.search('ssdp:all')
-}, 5000)
-
-// And after 10 seconds, you want to stop
-// setTimeout(function () {
-//   client.stop()
-// }, 10000)
+}, 5000);//Searching every 5 sec
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 var Server = require('node-ssdp').Server
-, server = new Server();
+, server = new Server({
+	location: require('ip').address()
+});
 
 server.addUSN('upnp:rootdevice');
 server.addUSN('urn:schemas-upnp-org:device:Presentation:1');
 
 server.on('advertise-alive', function(){
-	
+
 });
 
 server.on('advertise-bye', function(){
-
+	console.log('bye');
 });
 
-server.start('0.0.0.0');
+server.start('0.0.0.0');//Ganti dengan server 10.10.101.202
 
 process.on('exit', function(){
 	server.stop()
